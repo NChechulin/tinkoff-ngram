41208ce650166403a8a6377c81ce5f8e47384467
Nikolay Chechulin <nchechulin@protonmail.com>
2022-09-09 21:33:16 +0300

    add Text file class which holds sentences

--- tinkoff_ngram/parser.py
+++ tinkoff_ngram/parser.py
@@ -37,9 +37,38 @@ class Sentence:
         return "".join(
             filter(
                 lambda c: c in VALID_CHARACTERS,
-                sentence.lower(),
+                sentence.lower().replace("ё", "е"),
             )
         ).strip()
 
     def __len__(self) -> int:
         return len(self.words)
+
+
+@dataclass
+class TextFile:
+    """
+    Opens a file, cleans up the contents and parses the text into sentences.
+    """
+
+    input_file: Path
+    sentences: list[Sentence] = field(default_factory=list)
+
+    def __post_init__(self):
+        text = self.__get_text()
+        self.sentences = list(
+            map(lambda sentence: Sentence(sentence), self.__split_into_sentences(text))
+        )
+
+    def __get_text(self) -> str:
+        """
+        Reads text from the file and returns it
+        """
+        text = ""
+        with open(self.input_file, "r") as fh:
+            text = fh.read()
+        return text
+
+    def __split_into_sentences(self, text: str) -> list[str]:
+        text = " ".join(text.splitlines())
+        return text.split(".")
